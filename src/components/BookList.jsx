3e47ceabae67979b7acb1f8bdf732daf45e8fa78
unknown <sanchit2005yudavanshi@gmail.com>
2023-11-27 19:00:22 +0530

    changed the class component into functional component

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,10 +1,9 @@
-import { Component } from "react";
+import {useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
-class BookList extends Component {
-  static contextType = ThemeContext
-  render() {
-    const { isLightTheme, light, dark } = this.context;
+function BookList() {
+
+    const {isLightTheme, light, dark} = useContext(ThemeContext)
   // this is deciding the theme if the isLightTheme is true then the theme will be light and all the properties that light state have theme will have all those properties otherwise it will have dark theme and have all the properties that the dark state have.
   const theme = isLightTheme ? dark: light;
     return (
@@ -18,6 +17,6 @@ class BookList extends Component {
       </div>
     );
   }
-}
+
 
 export default BookList;
