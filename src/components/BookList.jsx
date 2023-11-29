23c5d8eda3141c689f5c814d97ae716ad7580c8a
unknown <sanchit2005yudavanshi@gmail.com>
2023-11-29 18:19:29 +0530

    added book context in book list component

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,22 +1,26 @@
-import {useContext } from "react";
+import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
+import { BookContext } from "../context/BookContext";
 
 function BookList() {
-
-    const {isLightTheme, light, dark} = useContext(ThemeContext)
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { books } = useContext(BookContext);
   // this is deciding the theme if the isLightTheme is true then the theme will be light and all the properties that light state have theme will have all those properties otherwise it will have dark theme and have all the properties that the dark state have.
-  const theme = isLightTheme ? dark: light;
-    return (
-      
-      <div className="book-list" style={{background: theme.bg, color: theme.syntax}}>
-        <ul>
-          <li style={{background: theme.ui}}>Godan</li>
-          <li style={{background: theme.ui}}>gaban</li>
-          <li style={{background: theme.ui}}>thele pe himalay</li>
-        </ul>
-      </div>
-    );
-  }
-
+  const theme = isLightTheme ? dark : light;
+  return (
+    <div
+      className="book-list"
+      style={{ background: theme.bg, color: theme.syntax }}
+    >
+      <ul>
+        {books.map((book) => (
+          <li style={{ background: theme.ui }} key={book.id}>
+            {book.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default BookList;
